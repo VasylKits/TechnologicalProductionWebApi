67ce3d3d865851480d4d6d9fc160b89f5ea0b194
vasyl <kitsulavasyl@gmail.com>
2023-03-02 16:28:55 +0200

    Add swagger button

--- TechnologicalProductionWebApi/Program.cs
+++ TechnologicalProductionWebApi/Program.cs
@@ -3,6 +3,7 @@ using Microsoft.EntityFrameworkCore;
 using TechnologicalProductionWebApi.Extensions;
 using TechnologicalProductionWebApi.Services.Interfaces;
 using TechnologicalProductionWebApi.Services.Implementations;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -19,7 +20,33 @@ builder.Services.AddTransient<IContractService, ContractService>();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Api Key Auth", Version = "v1" });
+    c.AddSecurityDefinition("ApiKey", new OpenApiSecurityScheme
+    {
+        Description = "ApiKey must appear in header",
+        Type = SecuritySchemeType.ApiKey,
+        Name = "ApiKey",
+        In = ParameterLocation.Header,
+        Scheme = "ApiKeyScheme"
+    });
+    var key = new OpenApiSecurityScheme()
+    {
+        Reference = new OpenApiReference
+        {
+            Type = ReferenceType.SecurityScheme,
+            Id = "ApiKey"
+        },
+        In = ParameterLocation.Header
+    };
+    var requirement = new OpenApiSecurityRequirement
+                    {
+                             { key, new List<string>() }
+                    };
+    c.AddSecurityRequirement(requirement);
+});
 
 var app = builder.Build();
 
